9252dd737d790d93cd0bc6b27d4bd0c4bedee16c
Максим Титов <matitov2004@gmail.com>
2024-09-06 23:39:22 +0300

    recommender in progress

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -28,7 +28,7 @@ class Recommender:
     def suggest_products_for(self, products: list[Product], max_results=6):
         product_ids = [product.id for product in products]
         if len(products) == 1:
-            suggestions = r.zrange(
+            suggestions = redis_conn.zrange(
                 self.get_product_key(product_ids[0]),
                 0,
                 -1,
@@ -38,3 +38,21 @@ class Recommender:
             flat_ids = ''.join([str(id) for id in product_ids])
             tmp_key = f'tmp_{flat_ids}'
             keys = [self.get_product_key(id) for id in product_ids]
+            redis_conn.zunionstore(tmp_key, keys)
+            redis_conn.zrem(tmp_key, *product_ids)
+            suggestions = redis_conn.zrange(
+                tmp_key, 0, -1, desc=True
+            )[:max_results]
+            redis_conn.delete(tmp_key)
+        suggested_products_ids = [int(id) for id in suggestions]
+        suggested_products = list(
+            Product.objects.filter(id__in=suggested_products_ids)
+        )
+        suggested_products.sort(
+            key=lambda x: suggested_products_ids.index(x.id)
+        )
+        return suggested_products
+
+    def clear_purchases(self):
+        for id in Product.objects.values_list('id', flat=True):
+            redis_conn.delete(self.get_product_key(id))
